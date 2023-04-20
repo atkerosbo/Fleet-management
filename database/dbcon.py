a03a5d9eaa59e08a3a1722e696bbcd883ee10af7
atkerosbo <109671579+atkerosbo@users.noreply.github.com>
2023-04-20 15:59:07 +0200

    0.05

--- database/dbcon.py
+++ database/dbcon.py
@@ -1,15 +1,54 @@
-#conecting to the local database
-import pypyodbc as pyodbc
+import pyodbc as odbc
 
 #connect the server
-class dbconnection:
-    def __init__(self):
-        self.server = 'DESKTOP-HH6PBH8'
-        self.database = 'fleet_mng_vehicles'
-        self.username = 'fleetmng'
-        self.password = 'hubbabubba2020'
-        self.conn = pyodbc.connect('DRIVER={SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+self.password)
-        self.cursor = self.conn.cursor()
+conn_str = (r'DRIVER={SQL Server};'
+            r'SERVER=DESKTOP-HH6PBH8;'
+            r'DATABASE=fleet_mng_vehicles;'
+            r'Trusted_Connection=yes;'
+                    )
+conn = odbc.connect(conn_str)
+
+
+#reading the data from the database
+
+def read(conn,table):
+    print("Read")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM "+table+";")
+    for row in cursor:
+        print(f"row = {row}")
+
+
+#create
+
+def create(conn):
+    print("Create")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO vehicle VALUES (?,?,?,?,?,?,?);", (3, "Mercedes", 2017, 120000, 2, 1, 1))
+    conn.commit()
+    read(conn)   
+
+
+#update
+
+def update(conn):
+    print("Update")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO vehicle VALUES (?,?,?,?,?,?,?);", (4, "Iveco", 2019, 100000, 2000, 1000, 1))
+    conn.commit()
+    read(conn) 
+
+#delete
+def delete(conn):
+    print("Delete")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM vehicle WHERE vehicle_id = 4")
+    conn.commit()
+    read(conn)  
+
+
+read(conn,"vehicle")
+
 
 
 
