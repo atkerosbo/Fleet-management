25e1dbbd492e8312e23252dc6d96fa80fabcdc5c
atkerosbo <atkerosbo@yahoo.com>
2023-04-24 22:25:26 +0200

    pandas profiling

--- maintenence_schedule.py
+++ maintenence_schedule.py
@@ -1,18 +1,1 @@
-from vehicle_list import VehicleList
-#Highlight and display the vehicle that needs mainenece
-
-def get_vehicle_list():
-    vehicle_list = [VehicleList]
-    for vehicle in VehicleList:
-        vehicle_list.append(vehicle[0])
-    return vehicle_list
-
-def get_vehicle(vehicle_id):
-    for vehicle in vehicles:
-        if vehicle[0] == vehicle_id:
-            return vehicle
-        
-def get_vehicle_km(vehicle_id):
-    for vehicle in vehicles:
-        if vehicle[0] == vehicle_id:
-            return vehicle[3]
+#Scheduling with factory aproximation, and with model prediction
